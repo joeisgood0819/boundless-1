cfd7d1b52236b12ed7fc24d9ff4f2d8fd331d73f
Joe <joeisgood0819@gmail.com>
2023-12-24 11:29:36 +0800

    檔案連線

--- boundless_connect.php
+++ boundless_connect.php
@@ -1,14 +1,13 @@
 <?php
 $servername = "localhost";
-$username = "admin";
+$username = "Padmin";
 $password = "12345";
-$dbname = "boundless_db";
-    
+$dbname = "boundless";
+
 // Create connection
 $conn = new mysqli($servername, $username, $password, $dbname);
 // 檢查連線
 // -> :存取物件本身的屬性或方法
 if ($conn->connect_error) {
-  	die("連線失敗: " . $conn->connect_error);
+	die("連線失敗: " . $conn->connect_error);
 }
-?>
